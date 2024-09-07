9e474e07b5a250caa526488d3fd9792234c81150
ingta <ing.talero@gmail.com>
2024-09-06 19:58:47 -0500

    nuevaActulizacion

--- practicaHelloword/practicaHelloword/src/main/java/helloword/practicaHelloword/HelloWordController.java
+++ practicaHelloword/practicaHelloword/src/main/java/helloword/practicaHelloword/HelloWordController.java
@@ -1,16 +1,21 @@
 package helloword.practicaHelloword;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author Cristian Talero
+ */
 @RestController
-
+@RequestMapping("/api")
 public class HelloWordController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    @GetMapping("/hello/{nombre}")
+    public String hello(@PathVariable String nombre) {
+        return "Hello World! es un tema de tiempo"+nombre;
+    }
+    @PostMapping("/hola/{nombre}")
+    public  String salud(@PathVariable String nombre){
+        return "Hola mundo! es un tema de tiempo"+nombre;
     }
   }
 
