b155959aa1dd1ba064b0cebb33828435618e5d72
cristian9212 <62980195+cristian9212@users.noreply.github.com>
2024-09-27 22:44:03 -0500

    nuevaActulizacion

---
+++ src/main/java/helloword/practicaHelloword/PracticaHellowordApplication.java
@@ -0,0 +1,13 @@
+package helloword.practicaHelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaHellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaHellowordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/helloword/practicaHelloword/PracticaHellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package helloword.practicaHelloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaHellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
